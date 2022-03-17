ca924d87e9874294e2b841f55e65805aee8d4af4
Aishwarya Shrestha <aishwaryashrestha4064@gmail.com>
2022-03-16 22:45:34 -0400

    Added dapling and velocity

--- ICE-7/ViewController.swift
+++ ICE-7/ViewController.swift
@@ -18,12 +18,29 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+
+// normal
+//        UIView.animate(withDuration: 2.0) {
+//            self.myLabel.center.x += self.view.bounds.width
+//            self.myTextField.center.x += self.view.bounds.width
+//            self.myImageView.center.x += self.view.bounds.width
+//        }
         
-        UIView.animate(withDuration: 2.0) {
-           self.myLabel.center.x += self.view.bounds.width
-           self.myTextField.center.x += self.view.bounds.width
-           self.myImageView.center.x += self.view.bounds.width
-       }
+// include delays
+//        UIView.animate(withDuration: 3.4, delay: 2.3, options:
+//        [.repeat, .autoreverse], animations: {
+//            self.myLabel.center.x += self.view.bounds.width
+//            self.myTextField.center.x += self.view.bounds.width
+//            self.myImageView.center.x += self.view.bounds.width
+//        }, completion: nil)
+        
+
+        //dampling and velocity
+        UIView.animate(withDuration: 2.0, delay: 0.5, usingSpringWithDamping: 0.1, initialSpringVelocity: 0.5, options: [.repeat, .autoreverse], animations: {
+            self.myLabel.center.x += self.view.bounds.width
+            self.myTextField.center.x += self.view.bounds.width
+            self.myImageView.center.x += self.view.bounds.width
+        }, completion: nil)
     }
 
     
